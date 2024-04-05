ed37dec75532953d96ef1175e296c4d13cb729be
oalkin <163097277+oalkin@users.noreply.github.com>
2024-04-04 21:27:56 -0400

    4/4/2024

--- functions/ss_notch.m
+++
@@ -1,8 +0,0 @@
-function [y,buffer] = ss_notch(x,Omg0,r,gain,buffer)
-  a1 = -2*r*cos(Omg0);
-  a2 = r*r;
-  b1 = -2*cos(Omg0);
-  w = x-a1*buffer(1)-a2*buffer(2);      % Eqn. (/*\matexfileeqn{\ref{eq:}}*/)
-  y = (w+b1*buffer(1)+buffer(2))*gain;  % Eqn. (/*\matexfileeqn{\ref{eq:}}*/)
-  buffer = [w;buffer(1)];               % Update buffer  
-end
